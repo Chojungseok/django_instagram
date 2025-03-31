85b6e313719c9c922c8f45818930323609fa9a3f
Chojungseok <whwndtjr9712@naver.com>
2025-03-31 14:46:01 +0900

    logout func구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomAuthenticationForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
@@ -31,3 +33,7 @@ def login(request):
         'form':form,
     }
     return render(request, 'login.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('posts:index')
