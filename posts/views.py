1dab0c1878eba463c2a93b4013da6b542710460f
Chojungseok <whwndtjr9712@naver.com>
2025-03-31 10:35:31 +0900

    post read func 추가

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(reqeust):
+    posts = Post.objects.all()
+    context = {
+        'posts':posts,
+    }
+    return render(reqeust, 'index.html', context)
