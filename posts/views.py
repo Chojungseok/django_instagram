b4c8bb906c45f8728c9a394067b84b5f5914e85f
Chojungseok <whwndtjr9712@naver.com>
2025-03-31 11:54:21 +0900

    create func 구현

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def index(reqeust):
@@ -8,3 +9,17 @@ def index(reqeust):
         'posts':posts,
     }
     return render(reqeust, 'index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+    else:
+        form = PostForm()
+
+    context = {
+        'form' : form
+    }
+    return render(request, 'create.html', context)
